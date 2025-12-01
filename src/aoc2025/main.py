3edea672184fef4d82ea148cd2f0cd0e84b4aba9
crab-apple <12462619+crab-apple@users.noreply.github.com>
2025-12-01 17:54:28 +0100

    Start writing some scripts to create the boilerplate

--- src/aoc2025/main.py
+++ src/aoc2025/main.py
@@ -1,3 +1,16 @@
+from pathlib import Path
+
 def solve_all():
-    print("Hello!")
+    for day in _list_days():
+        print(day)
+
+def create_boilerplate():
+    for day in _list_days():
+        Path("./src/aoc2025/solvers/"+day).mkdir(parents=True, exist_ok=True)
+        open("./src/aoc2025/solvers/"+day+"/__init__.py", "a").close()
+        open("./src/aoc2025/solvers/"+day+"/problem1.py", "a").close()
+        open("./src/aoc2025/solvers/"+day+"/problem2.py", "a").close()
+
+def _list_days():
+    return map(lambda i:"day{:02}".format(i+1), range(12))
 
