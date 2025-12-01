0589272b0892ed08d0bca46c978724bef3ffc5b3
crab-apple <12462619+crab-apple@users.noreply.github.com>
2025-12-01 18:26:32 +0100

    Create skeleton for solver functions

--- src/aoc2025/main.py
+++ src/aoc2025/main.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import textwrap
 
 
 def solve_all():
@@ -10,8 +11,16 @@ def create_boilerplate():
     for day in _list_days():
         Path("./src/aoc2025/solvers/" + day).mkdir(parents=True, exist_ok=True)
         open("./src/aoc2025/solvers/" + day + "/__init__.py", "a").close()
-        open("./src/aoc2025/solvers/" + day + "/problem1.py", "a").close()
-        open("./src/aoc2025/solvers/" + day + "/problem2.py", "a").close()
+        solver_content = textwrap.dedent(
+            """
+        def solve(input):
+            print("TODO")
+            """
+        ).lstrip()
+        with open("./src/aoc2025/solvers/" + day + "/problem1.py", "w") as f:
+            f.write(solver_content)
+        with open("./src/aoc2025/solvers/" + day + "/problem2.py", "w") as f:
+            f.write(solver_content)
 
 
 def _list_days():
---
+++ src/aoc2025/solvers/day06/problem2.py
@@ -0,0 +1,2 @@
+def solve(input):
+    print("TODO")
