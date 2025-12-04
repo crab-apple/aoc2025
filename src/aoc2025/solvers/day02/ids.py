91e38de8a1e05cf099fb93dcdd81802f79cfdb8e
crab-apple <12462619+crab-apple@users.noreply.github.com>
2025-12-04 05:26:42 +0100

    Some more refactoring of the invalid ids

--- src/aoc2025/solvers/day02/ids.py
+++ src/aoc2025/solvers/day02/ids.py
@@ -19,14 +19,17 @@ def find_invalid_ids(rge):
 def next_invalid_id(number):
     """Returns the lowest invalid ID that is equal or greater to the given number."""
 
-    if not _is_even(_num_digits(number)):
-        return _repeat(math.floor(math.pow(10, _num_digits(number) // 2)))
+    number1 = _num_digits(number)
+    if not number1 % 2 == 0:
+        return InvalidId(
+            (math.floor(math.pow(10, _num_digits(number) // 2))), 2
+        ).as_int()
 
-    first_half = _extract_first_half(number)
-    second_half = _extract_second_half(number)
-    if first_half < second_half:
-        return InvalidId(first_half + 1, 2).as_int()
-    return InvalidId(first_half, 2).as_int()
+    first_part = _extract_first_part(number)
+    invalid_id_from_first_part = InvalidId(first_part, 2)
+    if invalid_id_from_first_part.as_int() < number:
+        return InvalidId(first_part + 1, 2).as_int()
+    return InvalidId(first_part, 2).as_int()
 
 
 def prev_invalid_id(number):
@@ -34,7 +37,7 @@ def prev_invalid_id(number):
 
     Returns None if there is no invalid ID that satisfies these conditions"""
 
-    half = _extract_first_half(next_invalid_id(number))
+    half = _extract_first_part(next_invalid_id(number))
 
     if InvalidId(half, 2).as_int() == number:
         return number
@@ -44,23 +47,10 @@ def prev_invalid_id(number):
     return InvalidId(half - 1, 2).as_int()
 
 
-def _extract_first_half(number):
+def _extract_first_part(number, num_parts=2):
     num_digits = _num_digits(number)
-    return int(str(number)[0 : (num_digits // 2)])
-
-
-def _extract_second_half(number):
-    num_digits = _num_digits(number)
-    return int(str(number)[(num_digits // 2) : num_digits])
-
-
-def _repeat(number):
-    return int(str(number) + str(number))
+    return int(str(number)[0 : (num_digits // num_parts)])
 
 
 def _num_digits(number):
     return len(str(number))
-
-
-def _is_even(number):
-    return number % 2 == 0
