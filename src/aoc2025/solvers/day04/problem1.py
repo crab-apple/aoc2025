97f5f1473538a23fefc9f2d09e22c9ca78c823f3
crab-apple <12462619+crab-apple@users.noreply.github.com>
2025-12-04 15:14:30 +0100

    Parse grid

--- src/aoc2025/solvers/day04/problem1.py
+++ src/aoc2025/solvers/day04/problem1.py
@@ -1,11 +1,17 @@
 def solve(problem_input):
     grid = RollGrid(problem_input)
+    print(grid)
     return grid.count_accessible()
 
 
 class RollGrid:
     def __init__(self, input_str):
-        pass
+        lines = input_str.splitlines()
+        self._height = len(lines)
+        self._width = len(lines[0])
 
     def count_accessible(self):
         return 13
+
+    def __str__(self):
+        return "Grid with width {w}, height {h}".format(w=self._width, h=self._height)
