6e787404c2a429334f2568f4e08d088b203774d6
crab-apple <12462619+crab-apple@users.noreply.github.com>
2025-12-04 05:28:20 +0100

    Return InvalidId object from next_invalid_id

--- src/aoc2025/solvers/day02/ids.py
+++ src/aoc2025/solvers/day02/ids.py
@@ -4,14 +4,14 @@ from src.aoc2025.solvers.day02.invalid_id import InvalidId
 
 
 def find_invalid_ids(rge):
-    first = next_invalid_id(rge[0])
+    first = next_invalid_id(rge[0]).as_int()
     last = prev_invalid_id(rge[1])
     result = []
     if last is None:
         return []
     while first <= last:
         result.append(first)
-        first = next_invalid_id(first + 1)
+        first = next_invalid_id(first + 1).as_int()
 
     return result
 
@@ -23,13 +23,13 @@ def next_invalid_id(number):
     if not number1 % 2 == 0:
         return InvalidId(
             (math.floor(math.pow(10, _num_digits(number) // 2))), 2
-        ).as_int()
+        )
 
     first_part = _extract_first_part(number)
     invalid_id_from_first_part = InvalidId(first_part, 2)
     if invalid_id_from_first_part.as_int() < number:
-        return InvalidId(first_part + 1, 2).as_int()
-    return InvalidId(first_part, 2).as_int()
+        return InvalidId(first_part + 1, 2)
+    return InvalidId(first_part, 2)
 
 
 def prev_invalid_id(number):
@@ -37,7 +37,7 @@ def prev_invalid_id(number):
 
     Returns None if there is no invalid ID that satisfies these conditions"""
 
-    half = _extract_first_part(next_invalid_id(number))
+    half = _extract_first_part(next_invalid_id(number).as_int())
 
     if InvalidId(half, 2).as_int() == number:
         return number
@@ -49,7 +49,7 @@ def prev_invalid_id(number):
 
 def _extract_first_part(number, num_parts=2):
     num_digits = _num_digits(number)
-    return int(str(number)[0 : (num_digits // num_parts)])
+    return int(str(number)[0: (num_digits // num_parts)])
 
 
 def _num_digits(number):
--- tests/day02/test_ids.py
+++ tests/day02/test_ids.py
@@ -10,22 +10,22 @@ from src.aoc2025.solvers.day02.ids import (
 class TestNextInvalidId(unittest.TestCase):
 
     def test_even_digits_equal_halves(self):
-        self.assertEqual(1212, next_invalid_id(1212))
+        self.assertEqual(1212, next_invalid_id(1212).as_int())
 
     def test_even_digits_first_half_greater(self):
-        self.assertEqual(654654, next_invalid_id(654321))
+        self.assertEqual(654654, next_invalid_id(654321).as_int())
 
     def test_even_digits_first_half_lower(self):
-        self.assertEqual(124124, next_invalid_id(123456))
+        self.assertEqual(124124, next_invalid_id(123456).as_int())
 
     def test_uneven_digits(self):
-        self.assertEqual(100100, next_invalid_id(12345))
+        self.assertEqual(100100, next_invalid_id(12345).as_int())
 
     def test_zero(self):
-        self.assertEqual(11, next_invalid_id(0))
+        self.assertEqual(11, next_invalid_id(0).as_int())
 
     def test_single_digit(self):
-        self.assertEqual(11, next_invalid_id(3))
+        self.assertEqual(11, next_invalid_id(3).as_int())
 
 
 class TestPrevInvalidId(unittest.TestCase):
