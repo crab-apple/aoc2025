088c5b8d56aa0649e25395602cd6491671de4d10
crab-apple <12462619+crab-apple@users.noreply.github.com>
2025-12-05 10:26:23 +0100

    Formatting

--- src/aoc2025/solvers/day05/problem1.py
+++ src/aoc2025/solvers/day05/problem1.py
@@ -12,6 +12,8 @@ def solve(problem_input):
 def parse_ingredient_database(problem_input):
     lines = problem_input.splitlines()
     blank_line = lines.index("")
-    ranges = map(lambda r: (int(r.split("-")[0]), int(r.split("-")[1])), lines[0:blank_line])
-    ingredients = map(lambda i: int(i), lines[blank_line + 1:])
+    ranges = map(
+        lambda r: (int(r.split("-")[0]), int(r.split("-")[1])), lines[0:blank_line]
+    )
+    ingredients = map(lambda i: int(i), lines[blank_line + 1 :])
     return list(ranges), list(ingredients)
