f51b7c01514439268284a685bf370ff117b38423
crab-apple <12462619+crab-apple@users.noreply.github.com>
2025-12-05 10:49:00 +0100

    Formatting

--- tests/day05/test_range_merge.py
+++ tests/day05/test_range_merge.py
@@ -6,25 +6,19 @@ from src.aoc2025.solvers.day05.range_merge import merge_ranges
 class TestRangeMerge(unittest.TestCase):
 
     def test_separate_ranges(self):
-        self.assertEqual(
-            [(1, 2), (3, 4)],
-            merge_ranges([(1, 2), (3, 4)])
-        )
+        self.assertEqual([(1, 2), (3, 4)], merge_ranges([(1, 2), (3, 4)]))
 
     def test_overlapping_ranges(self):
         self.assertEqual(
-            [(1, 10), (20, 30)],
-            merge_ranges([(1, 10), (20, 25), (22, 30)])
+            [(1, 10), (20, 30)], merge_ranges([(1, 10), (20, 25), (22, 30)])
         )
 
     def test_subsuming_ranges(self):
         self.assertEqual(
-            [(1, 10), (20, 30)],
-            merge_ranges([(1, 10), (20, 30), (22, 25)])
+            [(1, 10), (20, 30)], merge_ranges([(1, 10), (20, 30), (22, 25)])
         )
 
     def test_unordered_ranges(self):
         self.assertEqual(
-            [(1, 10), (20, 30)],
-            merge_ranges([(20, 30), (1, 10), (22, 25)])
+            [(1, 10), (20, 30)], merge_ranges([(20, 30), (1, 10), (22, 25)])
         )
