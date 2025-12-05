174ed7203026e41f40abfc478833e267fcb2da0c
crab-apple <12462619+crab-apple@users.noreply.github.com>
2025-12-05 10:21:23 +0100

    Parse input for day 5

--- src/aoc2025/solvers/day05/problem1.py
+++ src/aoc2025/solvers/day05/problem1.py
@@ -1,2 +1,13 @@
 def solve(problem_input):
+    ranges, ingredients = parse_ingredient_database(problem_input)
+    print(ranges)
+    print(ingredients)
     return 3
+
+
+def parse_ingredient_database(problem_input):
+    lines = problem_input.splitlines()
+    blank_line = lines.index("")
+    ranges = map(lambda r: (int(r.split("-")[0]), int(r.split("-")[1])), lines[0:blank_line])
+    ingredients = map(lambda i: int(i), lines[blank_line + 1:])
+    return list(ranges), list(ingredients)
