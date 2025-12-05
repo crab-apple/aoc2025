5d1cba22aa4515dcf6712e449c49cabf6dc6c485
crab-apple <12462619+crab-apple@users.noreply.github.com>
2025-12-05 11:06:53 +0100

    Measure solving times

--- src/aoc2025/main.py
+++ src/aoc2025/main.py
@@ -1,25 +1,30 @@
 import importlib
 import textwrap
+import time
 from pathlib import Path
 
 
 def solve_all():
     for day in _list_days():
-        with open("./inputs/" + day + "/input", "r") as file:
-            problem_input = file.read()
-
-        module = importlib.import_module("aoc2025.solvers." + day + ".problem1")
-        output = module.solve(problem_input)
-        if output != -1:
-            print(day + "-1:")
-            print("  " + str(output))
-
-        module = importlib.import_module("aoc2025.solvers." + day + ".problem2")
-        output = module.solve(problem_input)
-        if output != -1:
-            print(day + "-2:")
-            print("  " + str(output))
+        _solve_and_print(day, 1)
+        _solve_and_print(day, 2)
         print("")
+
+
+def _solve_and_print(day, half):
+    with open("./inputs/" + day + "/input", "r") as file:
+        problem_input = file.read()
+    module = importlib.import_module(
+        "aoc2025.solvers." + day + ".problem{}".format(half)
+    )
+    time_a = time.perf_counter()
+    output = module.solve(problem_input)
+    time_b = time.perf_counter()
+    if output != -1:
+        print(day + "-1:")
+        print(
+            "  {} ({:.3f} seconds)".format(str(output).ljust(20, " "), time_b - time_a)
+        )
 
 
 def create_boilerplate():
