267724280befdfc5219c1609a778110d5ee05ceb
crab-apple <12462619+crab-apple@users.noreply.github.com>
2025-12-06 12:54:52 +0100

    Day 6-1 done

--- src/aoc2025/solvers/day06/problem1.py
+++ src/aoc2025/solvers/day06/problem1.py
@@ -1,2 +1,33 @@
+import re
+
+
 def solve(problem_input):
-    return -1
+    lines = parse_input(problem_input)
+
+    result = 0
+    for i in range(0, len(lines[0])):
+        operators = []
+        for line_num in range(0, len(lines) - 1):
+            operators.append(int(lines[line_num][i]))
+        symbol = lines[-1][i]
+        if symbol == "+":
+            result += sum(operators)
+        else:
+            result += mul(operators)
+
+    return result
+
+
+def mul(operators):
+    result = 1
+    for operator in operators:
+        result *= operator
+    return result
+
+
+def parse_input(problem_input):
+    return list(map(lambda l: _split_line(l), problem_input.splitlines()))
+
+
+def _split_line(l):
+    return re.split(r" +", l.strip())
--- tests/day06/test_problem1.py
+++ tests/day06/test_problem1.py
@@ -1,16 +1,18 @@
-from aoc2025.solvers.day06.problem1 import solve
 import textwrap
 import unittest
 
+from src.aoc2025.solvers.day06.problem1 import solve
 
-@unittest.skip("future problem")
+
 class TestProblem(unittest.TestCase):
 
     def test_example_input(self):
         example_input = textwrap.dedent(
             """
-        EXAMPLE
-        INPUT
+            123 328  51 64 
+             45 64  387 23 
+              6 98  215 314
+            *   +   *   +  
         """
         ).strip()
-        self.assertEqual("TODO EXPECTED OUTPUT", solve(example_input))
+        self.assertEqual(4277556, solve(example_input))
