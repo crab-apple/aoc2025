8d06ecdf9cbc7c563ac71804c8d05fb8a176af64
crab-apple <12462619+crab-apple@users.noreply.github.com>
2025-12-06 22:20:57 +0100

    Day 6-2 done

--- src/aoc2025/solvers/day06/problem2.py
+++ src/aoc2025/solvers/day06/problem2.py
@@ -1,2 +1,9 @@
+from src.aoc2025.solvers.day06.parsing import parse_input
+
+
 def solve(problem_input):
-    return -1
+    result = 0
+    for operation in parse_input(problem_input):
+        result += operation.uncephalopodize().execute()
+
+    return result
--- tests/day06/test_problem2.py
+++ tests/day06/test_problem2.py
@@ -1,16 +1,17 @@
-from aoc2025.solvers.day06.problem2 import solve
+from src.aoc2025.solvers.day06.problem2 import solve
 import textwrap
 import unittest
 
 
-@unittest.skip("future problem")
 class TestProblem(unittest.TestCase):
 
     def test_example_input(self):
         example_input = textwrap.dedent(
             """
-        EXAMPLE
-        INPUT
+            123 328  51 64 
+             45 64  387 23 
+              6 98  215 314
+            *   +   *   +  
         """
         ).strip()
-        self.assertEqual("TODO EXPECTED OUTPUT", solve(example_input))
+        self.assertEqual(3263827, solve(example_input))
