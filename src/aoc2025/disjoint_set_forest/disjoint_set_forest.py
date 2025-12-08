5c6c1893d0fb132ad772a5aec7df83ad74f7e350
crab-apple <12462619+crab-apple@users.noreply.github.com>
2025-12-08 09:54:57 +0100

    Implement union by rank
    Although I can't actually measure any improvement

--- src/aoc2025/disjoint_set_forest/disjoint_set_forest.py
+++ src/aoc2025/disjoint_set_forest/disjoint_set_forest.py
@@ -2,17 +2,30 @@ class DisjointSetForest:
     def __init__(self, size):
         self._nodes = []
         for i in range(0, size):
-            self._nodes.append(i)
+            self._nodes.append(Node(i, 0))
 
     def find(self, i):
-        parent = self._nodes[i]
+        parent = self._nodes[i].parent
         if parent == i:
             return i
         root = self.find(parent)
-        self._nodes[i] = root
+        self._nodes[i].parent = root
         return root
 
     def merge(self, a, b):
-        root_a = self.find(a)
-        root_b = self.find(b)
-        self._nodes[root_a] = root_b
+        root_node_a = self._nodes[self.find(a)]
+        root_node_b = self._nodes[self.find(b)]
+
+        if root_node_a.rank > root_node_b.rank:
+            root_node_a, root_node_b = root_node_b, root_node_a
+
+        root_node_a.parent = root_node_b.parent
+        if root_node_a.rank == root_node_b.rank:
+            root_node_a.parent = root_node_b.parent
+            root_node_b.rank += 1
+
+
+class Node:
+    def __init__(self, parent, rank):
+        self.parent = parent
+        self.rank = rank
