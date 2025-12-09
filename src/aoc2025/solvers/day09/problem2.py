d4c24780e2ab8a60d7ef4de051825f7201267fd0
crab-apple <12462619+crab-apple@users.noreply.github.com>
2025-12-09 08:34:04 +0100

    Some notes on how a solution might possibly work (I'm not sure)

--- src/aoc2025/solvers/day09/problem2.py
+++ src/aoc2025/solvers/day09/problem2.py
@@ -1,2 +1,14 @@
 def solve(problem_input):
+
+    # Classify edges between r-turning and l-turning
+    # Count number of turns either way to figure out overall direction of the loop
+    # Based on the direction, re-classify edges as convex or concave
+    # Compute all areas
+    # Sort by size descendant
+    # For each one, figure out whether there's a concave edge in the rectangle
+    #    (it must be fully in, being at the edge doesn't count)
+    #    (to make this possible, it may be necessary to:
+    #    * make a collection including the concave edges only
+    #    * index by one of the two coordinates (tree-set or so)
+
     return -1
