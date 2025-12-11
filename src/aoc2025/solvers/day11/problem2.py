56ee3670b9348bd67bd02386464d0169653ba694
crab-apple <12462619+crab-apple@users.noreply.github.com>
2025-12-11 09:14:34 +0100

    Solve day 11-2

--- src/aoc2025/solvers/day11/problem2.py
+++ src/aoc2025/solvers/day11/problem2.py
@@ -1,2 +1,61 @@
+from collections import namedtuple
+
+
 def solve(problem_input):
-    return -1
+    output_graph = _parse_input(problem_input)
+
+    input_graph = {}
+    for device in output_graph:
+        for output in output_graph[device]:
+            if output not in input_graph:
+                input_graph[output] = set()
+            input_graph[output].add(device)
+
+    result = _count_paths("out", input_graph)
+
+    return result.both
+
+
+def _count_paths(destination, input_graph):
+    cache = {}
+    return _count_paths_memoized(destination, input_graph, cache)
+
+
+def _count_paths_memoized(destination, input_graph, cache):
+    if destination == "svr":
+        return PathCount(dac=0, fft=0, both=0, none=1)
+    if destination not in input_graph:
+        return PathCount(dac=0, fft=0, both=0, none=0)
+    if destination in cache:
+        return cache[destination]
+
+    result = PathCount(0, 0, 0, 0)
+    for input in input_graph[destination]:
+        upstream = _count_paths_memoized(input, input_graph, cache)
+        result = PathCount(
+            dac=result.dac + upstream.dac,
+            fft=result.fft + upstream.fft,
+            both=result.both + upstream.both,
+            none=result.none + upstream.none,
+        )
+
+    if destination == "dac":
+        result = PathCount(dac=result.none, fft=0, both=result.fft, none=0)
+
+    if destination == "fft":
+        result = PathCount(dac=0, fft=result.none, both=result.dac, none=0)
+
+    cache[destination] = result
+    return result
+
+
+def _parse_input(problem_input):
+    graph = {}
+    for line in problem_input.splitlines():
+        device = line.split(":")[0]
+        outputs = set(line.split(":")[1].strip().split(" "))
+        graph[device] = outputs
+    return graph
+
+
+PathCount = namedtuple("PathCount", field_names=["dac", "fft", "both", "none"])
--- tests/day11/test_problem2.py
+++ tests/day11/test_problem2.py
@@ -1,16 +1,35 @@
-from aoc2025.solvers.day11.problem2 import solve
+import os
 import textwrap
 import unittest
 
+from src.aoc2025.solvers.day11.problem2 import solve
 
-@unittest.skip("future problem")
+
 class TestProblem(unittest.TestCase):
 
     def test_example_input(self):
         example_input = textwrap.dedent(
             """
-        EXAMPLE
-        INPUT
+            svr: aaa bbb
+            aaa: fft
+            fft: ccc
+            bbb: tty
+            tty: ccc
+            ccc: ddd eee
+            ddd: hub
+            hub: fff
+            eee: dac
+            dac: fff
+            fff: ggg hhh
+            ggg: out
+            hhh: out 
         """
         ).strip()
-        self.assertEqual("TODO EXPECTED OUTPUT", solve(example_input))
+        self.assertEqual(2, solve(example_input))
+
+    def test_real_input(self):
+        dirname = os.path.dirname(__file__)
+        input_file_path = os.path.join(dirname, "../../inputs/day11/input")
+        with open(input_file_path, "r") as file:
+            problem_input = file.read()
+        self.assertEqual(520476725037672, solve(problem_input))
