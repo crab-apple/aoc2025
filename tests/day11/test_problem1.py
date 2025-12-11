dddbf2feb39eb69b8715c60e706b44f79fd1d33c
crab-apple <12462619+crab-apple@users.noreply.github.com>
2025-12-11 08:30:42 +0100

    Prepare main test for day 11-1

--- tests/day11/test_problem1.py
+++ tests/day11/test_problem1.py
@@ -1,16 +1,32 @@
-from aoc2025.solvers.day11.problem1 import solve
+import os
 import textwrap
 import unittest
 
+from src.aoc2025.solvers.day11.problem1 import solve
 
-@unittest.skip("future problem")
+
 class TestProblem(unittest.TestCase):
 
     def test_example_input(self):
         example_input = textwrap.dedent(
             """
-        EXAMPLE
-        INPUT
+            aaa: you hhh
+            you: bbb ccc
+            bbb: ddd eee
+            ccc: ddd eee fff
+            ddd: ggg
+            eee: out
+            fff: out
+            ggg: out
+            hhh: ccc fff iii
+            iii: out
         """
         ).strip()
-        self.assertEqual("TODO EXPECTED OUTPUT", solve(example_input))
+        self.assertEqual(5, solve(example_input))
+
+    def test_real_input(self):
+        dirname = os.path.dirname(__file__)
+        input_file_path = os.path.join(dirname, "../../inputs/day11/input")
+        with open(input_file_path, "r") as file:
+            problem_input = file.read()
+        self.assertIsNotNone(solve(problem_input))
